7be20c929d2edf06b2172392f8c4f760edf83c98
felipe <felipe.rosa@projuris.com.br>
2021-03-10 01:35:11 -0300

    Solution Drawing Book

---
+++ Basic Programming/Drawing Book/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static int pageCount(int n, int p) {
+        int front = p >> 1;
+        int back =  ((n & 1) == 1)
+                ? (n - p) >> 1
+                : (n - p + 1) >> 1;
+        return Math.min(front, back);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int p = scan.nextInt();
+        scan.close();
+        int result = pageCount(n, p);
+        System.out.println(result);
+    }
+}
